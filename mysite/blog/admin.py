d955840b8834ed18f13683ef5d4aeb4aa248ce76
JokerSe <artuhovse@yandex.ru>
2024-06-23 03:29:48 +0300

    Extended model Post, added templates base, list and detail. The model Post has been registered in the administrative panel Django. Added urls.py

--- mysite/blog/admin.py
+++ mysite/blog/admin.py
@@ -1,3 +1,27 @@
 from django.contrib import admin
+from .models import Post
 
-# Register your models here.
+
+@admin.register(Post)
+class PostAdmin(admin.ModelAdmin):
+    """
+
+    @admin.register(Post) аналогично admin.site.register(Post)
+
+    PostAdmin - класс, который указывает как отображать модель в админке
+    list_display - какие поля отображаются в админке в виде столбцов
+    list_filter - добавление справа панели для фильтрации объектов
+    search_fields - по каким полям будет осуществляться поиск
+    prepopulated_fields - (предзаполненное поле) указывает что поле slug формируется в зависимости от title
+    raw_id_fields - представление поля в "сыром" виде, по id
+    date_hierarchy - иерархия, отображается ниже поля поиска
+    ordering - сортировка
+
+    """
+    list_display = ['title', 'slug', 'author', 'publish', 'status']
+    list_filter = ['status', 'created', 'publish', 'author']
+    search_fields = ['title', 'body']
+    prepopulated_fields = {'slug': ('title',)}
+    raw_id_fields = ['author']
+    date_hierarchy = 'publish'
+    ordering = ['status', 'publish']
--- mysite/blog/views.py
+++ mysite/blog/views.py
@@ -1,3 +1,31 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from django.http import Http404
 
-# Create your views here.
+from .models import Post
+
+
+def post_list(request):
+    posts = Post.published.all()
+    return render(request, "blog/post/list.html", {"posts": posts})
+
+
+def post_detail(request, id):
+    """
+    Функция-представление деталей поста
+
+    Аналог функции без использования get_objects_or_404()
+    try:
+        post = Post.publish.get(id=id)
+    except Post.DoesNotExist:
+        raise Http404("No post found")
+
+    :param request:
+    :param id:
+    :return:
+    """
+
+    post = get_object_or_404(Post,
+                             id=id,
+                             status=Post.Status.PUBLISHED)
+
+    return render(request, "blog/post/detail.html", {"post": post})
