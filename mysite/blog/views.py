f12285aed9e69c423dd9989dbf4b18bd8d9201b4
JokerSe <artuhovse@yandex.ru>
2024-06-24 01:22:03 +0300

    Chapter 2. Added method get_absolute_url of the Post model, the order of forming the URL address has been changed for post details, added pagination

--- mysite/blog/views.py
+++ mysite/blog/views.py
@@ -1,15 +1,20 @@
 from django.shortcuts import render, get_object_or_404
 from django.http import Http404
+from django.core.paginator import Paginator
 
 from .models import Post
 
 
 def post_list(request):
-    posts = Post.published.all()
+    post_list = Post.published.all()
+    # постраничная разбивка с 3 постами на страницу
+    paginator = Paginator(post_list, 3)
+    page_number = request.GET.get("page", 1)
+    posts = paginator.page(page_number)
     return render(request, "blog/post/list.html", {"posts": posts})
 
 
-def post_detail(request, id):
+def post_detail(request, year, month, day, post):
     """
     Функция-представление деталей поста
 
@@ -25,7 +30,10 @@ def post_detail(request, id):
     """
 
     post = get_object_or_404(Post,
-                             id=id,
-                             status=Post.Status.PUBLISHED)
+                             status=Post.Status.PUBLISHED,
+                             slug=post,
+                             publish__year=year,
+                             publish__month=month,
+                             publish__day=day)
 
     return render(request, "blog/post/detail.html", {"post": post})
