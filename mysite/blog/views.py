282340c8d314e32b8b644536825b9be2cb46131b
jokerse <artuhovse@yandex.ru>
2024-06-30 19:26:21 +0300

    Chapter 2.p130 Created CommentForm, added form to view, added comment_form.html and comment.html templates, added comments to post detail

--- mysite/blog/views.py
+++ mysite/blog/views.py
@@ -3,9 +3,10 @@ from django.http import Http404
 from django.views.generic import ListView
 from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 from django.core.mail import send_mail
+from django.views.decorators.http import require_POST
 
-from .models import Post
-from .forms import EmailPostForm
+from .models import Post, Comment
+from .forms import EmailPostForm, CommentForm
 
 
 class PostListView(ListView):
@@ -56,8 +57,13 @@ def post_detail(request, year, month, day, post):
                              publish__year=year,
                              publish__month=month,
                              publish__day=day)
-
-    return render(request, "blog/post/detail.html", {"post": post})
+    comments = post.comments.filter(active=True)
+    form = CommentForm()
+    return render(
+        request,
+        "blog/post/detail.html",
+        {"post": post, "comments": comments, "form": form}
+    )
 
 
 def post_share(request, post_id):
@@ -79,3 +85,24 @@ def post_share(request, post_id):
     else:
         form = EmailPostForm()
     return render(request, "blog/post/share.html", {"post": post, "form": form, 'sent': sent})
+
+
+@require_POST
+def post_comment(request, post_id):
+    post = get_object_or_404(Post,
+                             id=post_id,
+                             status=Post.Status.PUBLISHED)
+    comment = None
+
+    form = CommentForm(data=request.POST)
+    if form.is_valid():
+        #  Создать обьект класса Comment не сохраняя его в базе данных
+        comment = form.save(commit=False)
+        #  Назначить пост комментарию
+        comment.post = post
+        comment.save()
+    return render(
+        request,
+        "blog/post/comment.html",
+        {"post": post, "form": form, "comment": comment}
+    )
